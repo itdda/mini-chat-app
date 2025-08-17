d47b960f63abb1fd973830acf7cbe7676c2216cf
Yoon Jin <ssupershh@gmail.com>
2025-08-17 22:38:57 +0900

    pakage.json 커밋

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MiniChatRoom from './components/MiniChatRoom';
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [showChat, setShowChat] = useState(false);
+
+  useEffect(() => {
+      fetch(`${process.env.REACT_APP_API_URL}/hello`) // 서버 주소, 로컬에서는 보통 http://localhost:8080/hello
+        .then((res) => res.text()) // 문자열 받기
+        .then((data) => setMessage(data))
+        .catch((err) => console.error(err));
+    }, []);
+
+  return (
+    <>
+
+        <p>{message}</p> {/* 서버에서 받은 문자열 출력 */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<MiniChatRoom />} />
+          </Routes>
+        </Router>
+
+    </>
+  )
+}
+
+export default App
