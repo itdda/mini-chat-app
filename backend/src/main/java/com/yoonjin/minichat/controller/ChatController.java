9be06c79b575721050bc739550a3cb3d361d7c8e
Yoon Jin <ssupershh@gmail.com>
2025-08-17 23:34:20 +0900

    frontend url 경로 환경 설정 추가

--- backend/src/main/java/com/yoonjin/minichat/controller/ChatController.java
+++ backend/src/main/java/com/yoonjin/minichat/controller/ChatController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "${frontend.url}")
 public class ChatController {
 
     @GetMapping("/hello")
